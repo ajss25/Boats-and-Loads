5a7ac91ad49135924e0da1714ae6e246a21c7b56
ajss25 <alexshinjs@gmail.com>
2021-12-03 03:11:24 -0800

    Implement PUT /boats/boat_id

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def post_boats():
     return (jsonify(new_boat), 201)
 
 # get route for /boats/boat_id
-@app.route('/boats/<boat_id>', methods=['GET', 'PATCH'])
+@app.route('/boats/<boat_id>', methods=['GET', 'PATCH', 'PUT'])
 def get_boat(boat_id):
   boat_key = client.key(constants.boats, int(boat_id))
   boat = client.get(key=boat_key)
@@ -221,6 +221,52 @@ def get_boat(boat_id):
     boat["self"] = request.base_url
     boat["id"] = boat_id
     return (jsonify(boat), 200)
+  
+  elif request.method == 'PUT':
+    content = request.get_json()
+
+    # if the request is missing a JWT, return 401
+    if not request.headers.get('Authorization'):
+      return (jsonify({"Error": "The request object is missing a JWT or contains invalid JWT"}), 401)
+    
+    # get the JWT from the request
+    # reference: https://stackoverflow.com/questions/63518441/how-to-read-a-bearer-token-from-postman-into-python-code
+    request_JWT = request.headers.get('Authorization').split()[1]
+
+    # if the JWT is invalid, return 401
+    # reference: https://developers.google.com/identity/sign-in/web/backend-auth
+    try:
+      idinfo = id_token.verify_oauth2_token(request_JWT, google.auth.transport.requests.Request(), CLIENT_ID)
+      userid = idinfo['sub']
+    except ValueError:
+      return (jsonify({"Error": "The request object is missing a JWT or contains invalid JWT"}), 401)
+
+    # if the request contains accept header besides application/json, or is missing accept header, return 406
+    if 'application/json' not in request.accept_mimetypes:
+      return (jsonify({"Error": "MIME type not supported by the endpoint or the Accept header is missing"}), 406)
+
+    # if the boat does not exist, return 404
+    if not boat:
+      return (jsonify({"Error": "No boat with this boat_id exists"}), 404)
+
+    # if the boat does not belong to the user making the request, return 403
+    if boat["owner"] != userid:
+      return (jsonify({"Error": "The user making the request does not have access to this resource"}), 403)
+
+    # if the request does not provide exactly three attributes to edit, return 400
+    if len(content) != 3:
+      return (jsonify({"Error": "The request object is missing at least one of the required attributes"}), 400)
+
+    # put the boat and return 200
+    boat.update({"name": content["name"]})
+    boat.update({"type": content["type"]})
+    boat.update({"length": content["length"]})
+    client.put(boat)
+
+    boat["self"] = request.base_url
+    boat["id"] = boat_id
+    return (jsonify(boat), 200)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
