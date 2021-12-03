779eac275bae649e394717c144dd8c384747ab1f
ajss25 <alexshinjs@gmail.com>
2021-12-03 02:23:02 -0800

    Implement POST /boats

--- main.py
+++ main.py
@@ -90,5 +90,50 @@ def get_users():
     results = list(query.fetch())
     return (jsonify(results), 200)
 
+# post route for /boats
+@app.route('/boats', methods=['POST'])
+def post_boats():
+  if request.method == 'POST':
+    content = request.get_json()
+
+    # if the request is missing a JWT, return 401
+    if not request.headers.get('Authorization'):
+      return (jsonify({"Error": "The request object is missing a JWT or contains invalid JWT"}), 401)
+    
+    # get the JWT from the request
+		# reference: https://stackoverflow.com/questions/63518441/how-to-read-a-bearer-token-from-postman-into-python-code
+    request_JWT = request.headers.get('Authorization').split()[1]
+
+    # if the JWT is invalid, return 401
+    # reference: https://developers.google.com/identity/sign-in/web/backend-auth
+    try:
+      idinfo = id_token.verify_oauth2_token(request_JWT, google.auth.transport.requests.Request(), CLIENT_ID)
+      userid = idinfo['sub']
+    except ValueError:
+      return (jsonify({"Error": "The request object is missing a JWT or contains invalid JWT"}), 401)
+    
+    # if the request contains accept header besides application/json, or is missing accept header, return 406
+    if 'application/json' not in request.accept_mimetypes:
+      return (jsonify({"Error": "MIME type not supported by the endpoint or the Accept header is missing"}), 406)
+
+    # if the request is missing any of the required attributes, return 400
+    if "name" not in content or "type" not in content or "length" not in content:
+      return (jsonify({"Error": "The request object is missing at least one of the required attributes"}), 400)
+  
+    new_boat = datastore.entity.Entity(key=client.key(constants.boats))
+    new_boat.update({
+      "name": content["name"],
+      "type": content["type"],
+      "length": content["length"],
+      "owner": userid,
+      "loads": []
+    })
+    client.put(new_boat)
+
+    self_url = request.base_url + "/" + str(new_boat.key.id)
+    new_boat["id"] = new_boat.key.id
+    new_boat["self"] = self_url
+    return (jsonify(new_boat), 201)
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
